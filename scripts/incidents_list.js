dc4677b4ac618a5fbd696722600e731998463295
annamweber <weberamarie@gmail.com>
2018-10-28 07:27:32 -0400

    made changes to allow voting and tracking of upvotes'

--- scripts/incidents_list.js
+++ scripts/incidents_list.js
@@ -11,6 +11,15 @@ function populateTable(tableBody) {
   		var descrip = document.createTextNode(snapshot.val().description);
   		var categ = document.createTextNode(snapshot.val().category);
       var user = document.createTextNode(snapshot.val().user);
+      var votes = document.createTextNode(snapshot.val().upvotes);
+      var up = document.createTextNode('+')
+      var voteBtn = document.createElement('BUTTON');
+
+      voteBtn.appendChild(up);
+      var evntid = snapshot.val
+      voteBtn.setAttribute("id",evntid);
+      voteBtn.setAttribute("name","upvote")
+
   		
   		// Create table detail for each attribute
   		var dateRow = document.createElement("TD");
@@ -18,6 +27,8 @@ function populateTable(tableBody) {
   		var descripRow = document.createElement("TD");
   		var categRow = document.createElement("TD");
       var userRow =document.createElement("TD");
+      var voteRow = document.createElement("TD");
+      var buttonRow = document.createElement("TD");
 
   		// Link everything together
   		tableBody.appendChild(row);
@@ -26,14 +37,25 @@ function populateTable(tableBody) {
   		row.appendChild(descripRow);
   		row.appendChild(categRow);
       row.appendChild(userRow);
+      row.appendChild(voteRow);
+      row.appendChild(buttonRow);
   		dateRow.appendChild(date);
   		timeRow.appendChild(time);
   		descripRow.appendChild(descrip);
   		categRow.appendChild(categ);
       userRow.appendChild(user);
+      voteRow.appendChild(votes);
+      buttonRow.appendChild(voteBtn);
 	});
 }
 
 // Get table element
 var tableBody = document.getElementById("incidents-table-body");
 populateTable(tableBody);
+
+var voteBtns = document.getElementsByName("upvote");
+voteBtns.forEach(function(elem){
+   elem.addEventListener("click", function() {
+        console.log("upvoted");
+    });
+});
